4d187d265e13baecefc822cf9b6253601e14a373
kirill <borodach995@gmail.com>
2023-03-06 20:09:11 +0300

    final fix mentions

--- app.py
+++ app.py
@@ -7,14 +7,17 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 # Set up the OpenAI API client
-openai.api_key = "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"
+openai.api_key = "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"
 
 # Create the TeleBot client
 bot = telebot.TeleBot("5562426530:AAE09FoIxWIWRbGdL4ToJgDeqKh0YTY6esA")
 
+
+
 # Define a function to handle all messages
 @bot.message_handler(func=lambda message: True)
 def chat(message):
+    global response
     # Use the OpenAI API to generate a response
     response = openai.Completion.create(
         engine="text-davinci-002",
@@ -24,10 +27,14 @@ def chat(message):
         stop=None,
         temperature=0.5,
     ).choices[0].text
-
+    
     # Send the response to the user
-    if f'@{bot.get_me().username}' in message.text:
+    if message.chat.type == "group":
+        if f'@{bot.get_me().username}' in message.text:
+            bot.reply_to(message, response)
+    elif message.chat.type == "private":
         bot.reply_to(message, response)
+
 
 # Start the Bot
 bot.polling()
